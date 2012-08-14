148e8bf9463d0e1d26af88afbce9b1bc7ae455d5
Daniel O'Hanlon <dpohanlon@gmail.com>
2012-08-14 01:40:50 +0100

    Added some PE3 factor function, unfinished.

--- PE3.hs
+++ PE3.hs
@@ -1,10 +1,19 @@
 import Data.List
 
-main = 
-	let m = 1000 
-	in print (length(sieve m m [2,3..m]))
+main = print(sieve 1000)
 
-sieve :: Integer -> Integer -> [Integer] -> [Integer]
-sieve x m list
+sieve :: Integer -> [Integer]
+sieve input = 
+	let m = input
+	in sieve' m m [2,3..m]
+
+sieve' :: Integer -> Integer -> [Integer] -> [Integer]
+sieve' x m list
 	| x == 1 = list
-	| x > 1 = sieve (x - 1) m (list \\ [2*x, 3*x .. m])
+	| x > 1 = sieve' (x - 1) m (list \\ [2*x, 3*x .. m])
+
+factors :: Integer -> [Integer]
+factors n = 
+	let primes = sieve (floor (sqrt (fromIntegral n)))
+	in takeWhile(\x -> n `mod` x == 0) primes
+ 
